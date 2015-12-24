33f7998731147b0a834bcb15427f21ab1d38bbad
gi0baro <giovanni.barillari@gmail.com>
2015-12-24 19:38:39 +0100

    Handling 404 and 500 errors

--- weppyweb/controllers/main.py
+++ weppyweb/controllers/main.py
@@ -1,6 +1,16 @@
 from weppyweb import app, redis
 
 
+@app.on_error(404)
+def error_404():
+    return app.render_template("404.haml")
+
+
+@app.on_error(500)
+def error_500():
+    return app.render_template("500.haml")
+
+
 @app.expose("/")
 def index():
     version = redis.get("weppy:last_version") or "0.1 Altair"
