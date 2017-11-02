b26b3d51ddd5339e790cee57c134d5d869701c87
Giovanni Barillari <giovanni.barillari@gmail.com>
2017-11-02 18:58:30 +0100

    Added some meta

--- weppyweb/controllers/main.py
+++ weppyweb/controllers/main.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from weppy import response
 from .. import app, cache, redis
 from ..helpers.code_blocks import blocks
 
@@ -18,4 +19,7 @@ def error_500():
 @cache.response(query_params=False, language=False, duration=600)
 def index():
     version = redis.get("weppy:last_version") or "0.1 Altair"
+    response.meta.description = (
+        "weppy is a full-stack python web framework designed with simplicity "
+        "in mind")
     return {'version': version, 'code_blocks': blocks}
